f86ac28ee6cce09b534a417c56fb93db7ddffcde
linchast <54513537+linchast@users.noreply.github.com>
2019-10-06 13:29:54 -0400

    Resolving Conflicts

---
+++ app/public/api/certification.php
@@ -0,0 +1,26 @@
+[{
+  "certid": 1,
+  "certAgency": "American Heart Association",
+  "certName": "CPRx",
+  "expirationYears": 2
+}, {
+  "certid": 2,
+  "certAgency": "American Red Cross",
+  "certName": "CPRy",
+  "expirationYears": 1
+}, {
+  "certid": 3,
+  "certAgency": "Athens Technical College",
+  "certName": "Firefighterx",
+  "expirationYears": 2
+}, {
+  "certid": 4,
+  "certAgency": "Ivy Technical College",
+  "certName": "Firefighter y",
+  "expirationYears": 2
+}, {
+  "certid": 5,
+  "certAgency": "Georgia POST Academy",
+  "certName": "POST",
+  "expirationYears": 1
+}]
