41c20aa093578ac82fb27b31750ff7d410e90ae2
Soumya Garg <soumya.vi@gmail.com>
2019-10-13 23:48:03 -0400

    Changes in member and certificate pages

--- app/public/js/memberExtract.js
+++ app/public/js/memberExtract.js
@@ -10,16 +10,41 @@ var memberList = new Vue({
           isActive: "",
           gender: "",
           position: ""
-    }]
+    }],
+    recordMember : {}
   },
   methods: {
     fetchMembers() {
-      fetch('api/member.php')
+      fetch('api/member/index.php')
       .then(response => response.json())
       .then(json => {memberList.members = json});
+    },
+    handleSubmit(event) {
+      // fetch(url, {
+      //   method: 'post',
+      //   data: this.recordPatient
+      // })
+      // .then( ... )
+      this.members.push( this.recordMember );
+      this.handleReset();
+    },
+    handleReset() {
+      this.recordMember = {
+        firstName: "",
+        lastName: "",
+        radioNumber: "",
+        stationNumber: "",
+        isActive: "",
+        gender: "",
+        position: ""
+      }
+    },
+    handleRowClick(member) {
+      memberTriageApp.member = member;
     }
   },
   created() {
+    this.handleReset();
     this.fetchMembers();
   }
 })
